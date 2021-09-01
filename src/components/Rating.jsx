a0a8135bacea3e93a6faba0aae1b0bc48e6b7d6c
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-01 18:01:12 -0300

    correçao erro requisito 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,7 @@ class Rating extends React.Component {
     const { rating } = this.props;
     return (
       <div>
-        <p>{ rating }</p>
+        <span className="rating">{ rating }</span>
       </div>
     );
   }
