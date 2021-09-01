51696c0829c539e2c30a33046e8d1a22412fa15b
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-01 14:32:22 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      Hora de codar! 🚀✍️
     </div>
   );
 }
