69eebdbfa812556a06bd14e3eb2c8cd3a9672002
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-08 16:25:50 -0300

    n entendo pq n passa requisito 19

---
+++ src/components/teste.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-key */
+// eslint-disable-next-line no-unused-expressions
+// eslint-disable-next-line react/jsx-key
+// eslint-disable-next-line no-unused-expressions
+// eslint-disable-next-line react/jsx-indent
+<>
+  <div class="movie-card" data-testid="movie-card">
+    <img alt="Movie Cover" class="movie-card-image" src="images/movie_1" />
+    <div class="movie-card-body">
+      <h4 class="movie-card-title" data-testid="movie-card-title">An awesome title</h4>
+      <h5 class="movie-card-subtitle">Movie Subtitle 1</h5>
+      <p class="movie-card-storyline">Movie Storyline 1</p>
+    </div>
+    <div class="movie-card-rating" data-testid="rating">
+      <span class="rating">4.5</span>
+    </div>
+  </div>
+  <div class="movie-card" data-testid="movie-card">
+    <img alt="Movie Cover" class="movie-card-image" src="images/movie_2" />
+    <div class="movie-card-body">
+      <h4 class="movie-card-title" data-testid="movie-card-title">Movie Title 2</h4>
+      <h5 class="movie-card-subtitle">An incredible subtitle</h5>
+      <p class="movie-card-storyline">Movie Storyline 2</p>
+    </div>
+    <div class="movie-card-rating" data-testid="rating">
+      <span class="rating">4.5</span>
+    </div>
+  </div>
+  <div class="movie-card" data-testid="movie-card">
+    <img alt="Movie Cover" class="movie-card-image" src="images/movie_3" />
+    <div class="movie-card-body">
+      <h4 class="movie-card-title" data-testid="movie-card-title">Movie Title 3</h4>
+      <h5 class="movie-card-subtitle">Movie Subtitle 3</h5>
+      <p class="movie-card-storyline">An great storyline</p>
+    </div>
+    <div class="movie-card-rating" data-testid="rating">
+      <span class="rating">3</span>
+    </div>
+  </div>
+  <div class="movie-card" data-testid="movie-card">
+    <img alt="Movie Cover" class="movie-card-image" src="" />
+    <div class="movie-card-body">
+      <h4 class="movie-card-title" data-testid="movie-card-title">Harry Potter VII</h4>
+      <h5 class="movie-card-subtitle">Harry Potter magical subtitle</h5>
+      <p class="movie-card-storyline">Harry dies</p>
+    </div>
+    <div class="movie-card-rating" data-testid="rating">
+      <span class="rating">4.9</span>
+    </div>
+  </div>
+</>
